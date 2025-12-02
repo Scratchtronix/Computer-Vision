26080ddc7f34541ca29b13fda7401a80b78a0ac1
Scratchtronix <scratchroboticscom@gmail.com>
2025-12-02 17:01:54 +0530

    Create OCR Translation.py

---
+++ OCR Translation.py
@@ -0,0 +1,16 @@
+import pytesseract
+import requests
+from openai import OpenAI
+
+text = pytesseract.image_to_string("YOUR-SAMPLE-IMAGE")
+print(text)
+key = OpenAI(api_key="PASTE-IN-YOUR-API-KEY")
+lang = input("Please enter the language you want to translate this text to: ")
+response = key.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role" : "system", "content" : "You are a translator."},
+        {"role" : "user", "content" : f"Translate this text and return only the translated text in the language {lang}, not anything else-{text}."}
+    ]
+)
+print(response.choices[0].message["content"])
